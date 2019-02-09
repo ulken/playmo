3ccc8d44664c79c2da00f05bef9590fdcd835eaf
Oskar Löfgren <islander.abroad@gmail.com>
2019-02-10 00:04:50 +0100

    Dplay support

--- src/vendor/vendors.js
+++ src/vendor/vendors.js
@@ -3,5 +3,10 @@ export default [
     elementSelector: "#vjs_video_1_Clpp_html5_mse_api",
     keysToRegister: ["Space", "ArrowLeft", "ArrowRight"],
     matchURL: url => /^.*\.hbonordic\.com$/.test(url.hostname)
+  },
+  {
+    elementSelector: "#player",
+    keysToRegister: ["ArrowLeft", "ArrowRight"],
+    matchURL: url => /^.*\.dplay\..*$/.test(url.hostname)
   }
 ];
