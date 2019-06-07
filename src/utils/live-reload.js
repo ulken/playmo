124735a838a666c8e39f4a0ae62175484b60d383
Oskar Löfgren <islander.abroad@gmail.com>
2019-06-08 00:57:19 +0200

    :wrench: Add live reload of extension in dev mode (#15)

---
+++ src/utils/live-reload.js
@@ -0,0 +1,56 @@
+const flatten = arr =>
+  arr.reduce((acc, e) => acc.concat(Array.isArray(e) ? flatten(e) : e), []);
+
+const sum = xs => xs.reduce((acc, x) => acc + x, 0);
+
+const filesInDirectory = dir =>
+  new Promise(resolve =>
+    dir.createReader().readEntries(async entries => {
+      const files = await Promise.all(
+        entries
+          .filter(e => !e.name.startsWith("."))
+          .map(e =>
+            e.isDirectory
+              ? filesInDirectory(e)
+              : new Promise(resolve => e.file(resolve))
+          )
+      );
+      return resolve(flatten(files));
+    })
+  );
+
+const modifiedTimeForFilesInDirectory = async dir => {
+  const files = await filesInDirectory(dir);
+  return sum(files.map(f => f.lastModified));
+};
+
+const reload = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => {
+    if (activeTab) {
+      chrome.tabs.reload(activeTab.id);
+    }
+
+    chrome.runtime.reload();
+  });
+};
+
+const watchForChanges = async (dir, lastModifiedTime) => {
+  const modifiedTime = await modifiedTimeForFilesInDirectory(dir);
+  const hasChanged = lastModifiedTime && lastModifiedTime !== modifiedTime;
+  if (hasChanged) {
+    reload();
+  } else {
+    setTimeout(() => watchForChanges(dir, modifiedTime), 100);
+  }
+};
+
+const watch = () =>
+  chrome.management.getSelf(self => {
+    if (self.installType === "development") {
+      chrome.runtime.getPackageDirectoryEntry(watchForChanges);
+    }
+  });
+
+const LiveReload = { watch };
+
+export default LiveReload;
